c2a1330e8f4ac6dc115f66a084e543f93f0f57c8
yaoh <yaoh.yao@beacool.com>
2019-08-09 13:27:49 +0800

    学习组件化 增加ARouter使用

---
+++ app/src/main/java/com/yaoh/compont/MainActivity.java
@@ -0,0 +1,25 @@
+package com.yaoh.compont;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.yaoh.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(view -> {
+//            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            ARouter.getInstance().build("/account/login").navigation();
+        });
+    }
+}
---
+++ login/src/main/java/com/yaoh/login/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.yaoh.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+@Route(path = "/account/login")
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+
+}
